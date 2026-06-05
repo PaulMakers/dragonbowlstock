19a289e9491fada4633d25afb4e01a37afa5c650
Rahmad aryadi <rahmadaryadi17@gmail.com>
2026-06-05 18:00:46 +0000

    ini link yang baru
    https://script.google.com/macros/s/AKfycbwJG3qFGW4EU

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxa9VzpWU36--sZv8ZUfVTZk2fDxL65zClbHgR5PHANDJulf-bmh2WS4ariZvHrddoF/exec';
+export const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwJG3qFGW4EUT0DIPB-8gYv4pK_fTZH2a3hdsY5DdOfXWIGYYE9AIbcXWDc97BKC_pV/exec';
 export const LOGO_URL = 'https://i.ibb.co.com/0R3MsqSp/Logo-Dragonbowl-removebg-preview.png';
 
 export const STATUS_OPTIONS = [
